5913c85cdd9c9f33c4ecef3fdf0609db7d77cc05
Alice Suckling <alicesuckling@live.co.uk>
2016-02-08 16:34:30 +0000

    refactor wiki policy to use queries

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -2,20 +2,13 @@ class WikiPolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-      wikis = []
       if @user.role == 'admin'
-        wikis = scope.all # if the user is an admin, show them all the wikis
+        scope.all 
       elsif user.role == 'premium'
-        all_wikis = scope.all
-        all_wikis.each do |wiki|
-          if wiki.public? || wiki.owner == user || wiki.collaborators.include?(user)
-            wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
-          end
-        end
-      else # this is the lowly standard user
-        wikis = scope.where(private: false)
+        scope.where('private = ? OR user_id = ?', false, user.id)
+      else # this is the lowly standard user and guest
+        scope.where(private: false)
       end
-      wikis # return the wikis array we've built up
     end
   end
 

bb92188ccbcfbb569f8472077eee25ff9967452c
Alice Suckling <alicesuckling@live.co.uk>
2016-02-08 16:15:48 +0000

    fixed wiki policy so standard users only see public wikis

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,35 +1,21 @@
 class WikiPolicy < ApplicationPolicy
 
-
-  class Scope
-    attr_reader :user, :scope
-
-    def initialize(user, scope)
-      @user = user
-      @scope = scope
-    end
-
+  class Scope < Scope
     def resolve
       wikis = []
-      if user.present? && user.role == 'admin'
-        wikis = scope.all
-      elsif user.present? && user.role == 'premium'
+      if @user.role == 'admin'
+        wikis = scope.all # if the user is an admin, show them all the wikis
+      elsif user.role == 'premium'
         all_wikis = scope.all
         all_wikis.each do |wiki|
-          if wiki.user == user && wiki != :private#|| wiki.user.include?(user)
-            wikis << wiki
+          if wiki.public? || wiki.owner == user || wiki.collaborators.include?(user)
+            wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
           end
         end
-      else
-        all_wikis = scope.all
-        wikis = []
-        all_wikis.each do |wiki|
-          if wiki != :private #|| wiki.user.include?(user)
-            wikis << wiki
-          end
-        end
+      else # this is the lowly standard user
+        wikis = scope.where(private: false)
       end
-      wikis
+      wikis # return the wikis array we've built up
     end
   end
 
